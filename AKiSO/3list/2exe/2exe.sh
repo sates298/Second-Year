4490ca8107bbab93fdc889ab6fa81311734589e4
sates298 <stanislaw.wo@interia.pl>
2018-11-09 00:56:31 +0100

    finish second exercise

--- AKiSO/3list/2exe/2exe.sh
+++ AKiSO/3list/2exe/2exe.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+clear
 cd /proc/
 
-find -maxdepth 1 -type d | for p in [0-9]*
-do printf "pid:"
-   printf $p"\t"
-   grep 'Name' $p/status
+for p in [0-9]*
+do
+  process_name=`grep "Name:" $p/status`
+  process_ppid=`grep "PPid:" $p/status`
+  opened_files=`sudo ls $p/fd | wc -l`
+  process_state=`grep "State:" $p/status`
+  echo -e "Pid: $p -\t $process_ppid -\t Files Amount: $opened_files -\t $process_state -\t $process_name"
+
 
 done
